a5af53f71b23b3d39be345b061323687eb4677e8
Yevhen Vydolob <yvydolob@redhat.com>
2023-05-04 16:09:47 +0300

    Add task for pushing images
    Signed-off-by: Yevhen Vydolob <yvydolob@redhat.com>

--- src/image-handler.ts
+++ src/image-handler.ts
@@ -28,32 +28,41 @@ export async function pushImageToCrcCluster(image: ImageInfo): Promise<void> {
   if (!image.name) {
     throw new Error('Image selection not supported yet');
   }
-
-  const filename = path.join(os.tmpdir(), image.name ? image.name.replaceAll('/', '') : image.engineId);
-  try {
-    let name = image.name;
-    if (image.tag) {
-      name = name + ':' + image.tag;
-    }
-    await extensionApi.containerEngine.saveImage(image.engineId, name, filename);
-    const result = await runCliCommand(getPodmanCli(), [
-      '--url=ssh://core@127.0.0.1:2222/run/podman/podman.sock',
-      `--identity=${os.homedir()}/.crc/machines/crc/id_ecdsa`,
-      'load',
-      '-i',
-      filename,
-    ]);
-
-    if (result.exitCode !== 0) {
-      throw new Error(result.stdErr);
-    }
-    extensionApi.window.showNotification({
-      body: `Image ${image.name} pushed to ${productName} cluster`,
-    });
-  } catch (error) {
-    const errorMessage = error instanceof Error ? error.message : '' + error;
-    throw new Error(`Error while pushing image ${image.name} to  ${productName} cluster: ${errorMessage}`);
-  } finally {
-    fs.promises.rm(filename);
+  let name = image.name;
+  if (image.tag) {
+    name = name + ':' + image.tag;
   }
+
+  return extensionApi.window.withProgress(
+    { location: extensionApi.ProgressLocation.TASK_WIDGET, title: `Pushing ${name}...` },
+    async progress => {
+      progress.report({ increment: 0 });
+      const filename = path.join(os.tmpdir(), image.name ? image.name.replaceAll('/', '') : image.engineId);
+      try {
+        await extensionApi.containerEngine.saveImage(image.engineId, name, filename);
+        progress.report({ increment: 50 });
+        const result = await runCliCommand(getPodmanCli(), [
+          '--url=ssh://core@127.0.0.1:2222/run/podman/podman.sock',
+          `--identity=${os.homedir()}/.crc/machines/crc/id_ecdsa`,
+          'load',
+          '-i',
+          filename,
+        ]);
+        progress.report({ increment: 100 });
+        if (result.exitCode !== 0) {
+          throw new Error(result.stdErr);
+        }
+        extensionApi.window.showNotification({
+          body: `Image ${image.name} pushed to ${productName} cluster`,
+        });
+      } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : '' + error;
+        progress.report({
+          message: `Error while pushing image ${image.name} to  ${productName} cluster: ${errorMessage}`,
+        });
+      } finally {
+        fs.promises.rm(filename);
+      }
+    },
+  );
 }
