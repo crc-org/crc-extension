ac3258ac692476a4ba3c1bd1f2ba04243842071b
Daniel Villanueva <davillan@redhat.com>
2025-02-11 12:46:55 +0100

    test(e2e): added deploying to crc cluster test case
    Signed-off-by: Daniel Villanueva <davillan@redhat.com>
    
    test(e2e): move test case to azure runner
    
    Signed-off-by: Daniel Villanueva <davillan@redhat.com>
    
    test(e2e): reorganized folder structure
    
    Signed-off-by: Daniel Villanueva <davillan@redhat.com>
    
    test(e2e): move test case to azure runner
    
    Signed-off-by: Daniel Villanueva <davillan@redhat.com>
    
    test(e2e): reorganized folder structure
    
    Signed-off-by: Daniel Villanueva <davillan@redhat.com>
    
    test(e2e): reverted folder reestructure, skip test on non-azure, non-windows runners
    
    Signed-off-by: Daniel Villanueva <davillan@redhat.com>
    
    chore(test): added timeout
    
    Signed-off-by: Daniel Villanueva <davillan@redhat.com>
    
    chore(test): modified script in package.json
    
    Signed-off-by: Daniel Villanueva <davillan@redhat.com>
    
    chore(test): removed ampersands
    
    Signed-off-by: Daniel Villanueva <davillan@redhat.com>
    
    chore(test): updated podman version
    
    Signed-off-by: Daniel Villanueva <davillan@redhat.com>

---
+++ tests/src/crc-cluster-deployment.spec.ts
@@ -0,0 +1,150 @@
+/**********************************************************************
+ * Copyright (C) 2025 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+import type { ContainerInteractiveParams } from '@podman-desktop/tests-playwright';
+import { expect as playExpect, RunnerOptions, test, ContainerState, ContainerDetailsPage, deleteContainer, deleteImage, deletePod, isWindows } from '@podman-desktop/tests-playwright';
+
+const kubernetesContext = 'microshift';
+const imageName1 = 'quay.io/sclorg/httpd-24-micro-c9s';
+const imageName2 = 'ghcr.io/linuxcontainers/alpine';
+const containerName1 = 'container-to-deploy-1';
+const containerName2 = 'container-to-deploy-2';
+const deployedPodName1 = 'container-1-pod';
+const deployedPodName2 = 'container-2-pod';
+const containerStartParams: ContainerInteractiveParams = {
+  attachTerminal: false,
+};
+
+test.use({ 
+  runnerOptions: new RunnerOptions({ customFolder: 'crc-tests-pd', autoUpdate: false, autoCheckUpdates: false }),
+});
+test.beforeAll(async ({ runner, welcomePage }) => {
+  runner.setVideoAndTraceName('crc-cluster-deployment-e2e');
+  await welcomePage.handleWelcomePage(true);
+});
+
+test.afterAll(async ({ runner, page }) => {
+  try {
+    await deletePod(page, deployedPodName1);
+    await deletePod(page, deployedPodName2);
+    await deleteContainer(page, containerName1);
+    await deleteContainer(page, containerName2);
+    await deleteImage(page, imageName1);
+    await deleteImage(page, imageName2);
+  } finally {
+    await runner.close();
+    console.log('Runner closed');
+  }
+});
+
+test.describe.serial('Deployment to OpenShift Local cluster', () => {
+  
+  test.describe.serial('Deploy a container to a CRC cluster by pushing the image from Podman Desktop', () => {
+    test.skip(!!process.env.AZURE_RUNNER === false || !isWindows, 'This test should only run on a Windows Azure machine');
+    
+    test('Pull image 1 and start the container', async ({ navigationBar }) => {
+      const imagesPage = await navigationBar.openImages();
+      await playExpect(imagesPage.heading).toBeVisible();
+      
+      const pullImagePage = await imagesPage.openPullImage();
+      const updatedImages = await pullImagePage.pullImage(imageName1);
+      
+      const exists = await updatedImages.waitForImageExists(imageName1);
+      playExpect(exists, `${imageName1} image not present in the list of images`).toBeTruthy();
+      playExpect(await updatedImages.getCurrentStatusOfImage(imageName1)).toBe('UNUSED');
+
+      const containersPage = await imagesPage.startContainerWithImage(
+        imageName1,
+        containerName1,
+        containerStartParams,
+      );
+      await playExpect.poll(async () => containersPage.containerExists(containerName1)).toBeTruthy();
+      const containerDetails = await containersPage.openContainersDetails(containerName1);
+      await playExpect(containerDetails.heading).toBeVisible();
+      await playExpect.poll(async () => containerDetails.getState()).toBe(ContainerState.Running);
+    });
+
+    test.fail('Push the image to the cluster', async ({ navigationBar, statusBar, page }) => {
+      const imagesPage = await navigationBar.openImages();
+      const pulledImage = await imagesPage.getImageRowByName(imageName1);
+      if (pulledImage === undefined) {
+        throw Error(`Image: '${name}' does not exist`);
+      }
+      const kebabMenuButton = pulledImage.getByRole('button', { name: 'kebab menu' });
+      await playExpect(kebabMenuButton).toBeVisible();
+      await kebabMenuButton.click();
+      //This step will fail => [BUG] option to push the image to OpenShift not shown #372
+      const pushToClusterButton = imagesPage.getPage().getByTitle('Drop Down Menu Items').getByTitle('Push image to OpenShift Local cluster');
+      await playExpect(pushToClusterButton).toBeVisible();
+      //This step will fail => [BUG] Can't push images to OpenShift Local clusters (ssh key name issue) #495
+      await pushToClusterButton.click();
+      await statusBar.tasksButton.click();
+      const tasksManager = page.getByTitle('Tasks manager');
+      await playExpect(tasksManager.getByTitle('/^Image ${imageName1} was successfully pushed to the OpenShift Local cluster /')).toBeVisible(); //not the actual message; locally this appears only if the crc cluster is started
+    });
+
+    test.skip('Deploy the container to the crc cluster -- previous step expected to fail', async ({ page, navigationBar }) => {
+      await navigationBar.openContainers();
+      const containerDetailsPage = new ContainerDetailsPage(page, containerName1);
+      await playExpect(containerDetailsPage.heading).toBeVisible();
+      const deployToKubernetesPage = await containerDetailsPage.openDeployToKubernetesPage();
+      await deployToKubernetesPage.deployPod(deployedPodName1, { useKubernetesServices: true, isOpenShiftCluster: true, useOpenShiftRoutes: true }, kubernetesContext);
+    
+      const podsPage = await navigationBar.openPods();
+      await playExpect.poll(async () => podsPage.deployedPodExists(deployedPodName1, 'kubernetes')).toBeTruthy();
+    });
+
+  });
+
+  test.describe.serial('Deploy a container to a CRC cluster by pulling the image directly from the cluster', () => {
+    test.skip(!!process.env.AZURE_RUNNER === false || !isWindows, 'This test should only run on a Windows Azure machine');
+    
+    test('Pull image 2 and start a container', async ({ navigationBar }) => {
+      const imagesPage = await navigationBar.openImages();
+      await playExpect(imagesPage.heading).toBeVisible();
+      
+      const pullImagePage = await imagesPage.openPullImage();
+      const updatedImages = await pullImagePage.pullImage(imageName2);
+      
+      const exists = await updatedImages.waitForImageExists(imageName2);
+      playExpect(exists, `${imageName2} image not present in the list of images`).toBeTruthy();
+      playExpect(await updatedImages.getCurrentStatusOfImage(imageName2)).toBe('UNUSED');
+
+      const containersPage = await imagesPage.startContainerWithImage(
+        imageName2,
+        containerName2,
+        containerStartParams,
+      );
+      await playExpect.poll(async () => containersPage.containerExists(containerName2)).toBeTruthy();
+      const containerDetails = await containersPage.openContainersDetails(containerName2);
+      await playExpect(containerDetails.heading).toBeVisible();
+      await playExpect.poll(async () => containerDetails.getState()).toBe(ContainerState.Running);
+    });
+
+    test('Deploy the container to the crc cluster', async ({ page, navigationBar }) => {
+      const containerDetailsPage = new ContainerDetailsPage(page, containerName2);
+      await playExpect(containerDetailsPage.heading).toBeVisible();
+      const deployToKubernetesPage = await containerDetailsPage.openDeployToKubernetesPage();
+      await deployToKubernetesPage.deployPod(deployedPodName2, { useKubernetesServices: true, isOpenShiftCluster: true, useOpenShiftRoutes: true  }, kubernetesContext);
+      
+      const podsPage = await navigationBar.openPods();
+      await playExpect.poll(async () => podsPage.deployedPodExists(deployedPodName2, 'kubernetes')).toBeTruthy();
+    });
+  });
+
+});
